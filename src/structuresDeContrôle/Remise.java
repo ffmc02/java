56512c26a70529e585dff5eac570fcc4d5e7a3c4
jonard <gaetan.jonard@outlook.fr>
2020-04-24 16:01:51 +0200

    remise a terminé

--- src/structuresDeContrôle/Remise.java
+++ src/structuresDeContrôle/Remise.java
@@ -4,11 +4,23 @@
  * and open the template in the editor.
  */
 package structuresDeContrôle;
-
+import java.util.Scanner;
 /**
  *
  * @author gaeta
  */
 public class Remise {
-    
+   public static void main(String args[]) {
+        System.out.println("Remise(V1.0, 24/04/2020)");
+        Scanner lectureclavier = new Scanner(System.in);
+        int PU;
+        int QTE;
+        int PAP;
+        int port;
+        double remise;
+        port= 6;
+        System.out.println("Mettre la quantité de l'article 1");
+        
+   }
+        
 }
