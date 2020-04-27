e5844f715dc1df53d9d4bb536ce3dda933ed3f7b
jonard <gaetan.jonard@outlook.fr>
2020-04-27 14:33:13 +0200

    Multiples

--- src/boucles/Multiples.java
+++ src/boucles/Multiples.java
@@ -4,11 +4,25 @@
  * and open the template in the editor.
  */
 package boucles;
-
+import java.util.Scanner; 
 /**
  *
  * @author gaeta
  */
 public class Multiples {
-    
+    public static void main(String args[]) {
+        System.out.println("Multiples(V1.0, 27/04/2020)");
+        Scanner lectureclavier = new Scanner(System.in);
+        int n;
+        int x;
+        int result;
+        System.out.println("Entré un premier nombre entier");
+        n= lectureclavier.nextInt();
+        System.out.println("Entré un second nombre entier");
+        x=  lectureclavier.nextInt();
+        for(int i=1 ; n>=i ; i++){
+            result = i*x;
+            System.out.println(x + " * " + i + "=" +result);
+        }
+    }
 }
