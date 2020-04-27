2372879c8cb83ca4d95881a839162519c6657458
jonard <gaetan.jonard@outlook.fr>
2020-04-27 15:55:06 +0200

    palindrome2

--- src/boucles/palindrome.java
+++ src/boucles/palindrome.java
@@ -16,14 +16,14 @@ public class palindrome {
     public static void main(String args[]) {
         System.out.println("Palindrom(V1.0, 27/04/2020)");
         Scanner lectureclavier = new Scanner(System.in);
-        System.out.print("Entrez un original ou une phrase : ");
+        System.out.print("Entrez un mot  ou une phrase : ");
         String original = lectureclavier.nextLine();
         // on enleve les majuscule pour évité les problémes de casse
         String test = original.toLowerCase();
         // je ne garde que  les caractères alphabétiques
         String allowedList = "abcdefghijklmnopqrstuvwxyz";
         String temp = "";
-        
+
         for (int i = 0; i < test.length(); i++) {
             char c = test.charAt(i);
             if (allowedList.indexOf(c) != -1) {
@@ -36,18 +36,28 @@ public class palindrome {
         int leftPos = 0;
         int rightPos = test.length() - 1;
         boolean palindrome = true;
+        //on vérifiez que la lettre la plus a gauche et la plus a droite sont les memes
         while ((leftPos < rightPos) && palindrome) {
+            //on teste si se n'est pas un palindrome alors
             if (test.charAt(leftPos) != test.charAt(rightPos)) {
+                //on passe la variable palindrome a false
                 palindrome = false;
             }
+            /**
+             * on continue temps que palindrome ne passe pas a false a ce
+             * raproche a chaque lettre-> en avancanr d'une lettre vers la droit
+             * en partant de la gauche et en reculant d'une lettre vers la
+             * gauche et en partant vers la droite
+             */
             leftPos++;
             rightPos--;
         }
-
+        //si palindrome est égale a true alors on dit que c'est un palindrome 
         if (palindrome) {
-            System.out.println("C'est un palindrôme !");
+            System.out.println("votre mot ou phrase est un palindrôme !!!!");
         } else {
-            System.out.println("Non, ce n'est pas un palindrôme.");
+            //si non on dit que ce n'est pas un palindrome 
+            System.out.println("votre mot ou phrase n'est pas un palindrôme!!!");
         }
     }
 
